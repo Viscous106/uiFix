201c3527975a440c2e100d39dd64f33a3548afd0
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-02-21 15:19:30 +0530

    [Ui]made changes in tailwind and html

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,4 @@
+//This is the background script that runs in the background and handles the extension logic.
 chrome.runtime.onInstalled.addListener(() => {
     console.log("uiFix Extension Installed");
 });
