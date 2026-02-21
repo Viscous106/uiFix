a3c8bd5eb40edfd92d64dbe41650419c3ca2bc02
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-02-21 14:39:12 +0530

    [Ui]made a basic extention

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,3 @@
-//This is the background script that runs in the background and handles the extension logic.
 chrome.runtime.onInstalled.addListener(() => {
     console.log("uiFix Extension Installed");
 });
